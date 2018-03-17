25c2c7308455e727433c9f71f7985b95ea196bd5
Esmanur <e_yildiz@icloud.com>
2018-03-18 02:16:06 +0300

    Add files via upload

--- 4.soru_ortalama_stok.py
+++ 4.soru_ortalama_stok.py
@@ -1,20 +1,20 @@
-koltukSayisi=int(input("Koltuk Sayısını giriniz: "))
-yatakSayisi=int(input("Yatak Sayısını giriniz: "))
-dolapSayisi=int(input("Dolap Sayısını giriniz: "))
+dbasi_koltukSayisi=int(input("Koltuk Sayısını giriniz: "))
+dbasi_yatakSayisi=int(input("Yatak Sayısını giriniz: "))
+dbasi_dolapSayisi=int(input("Dolap Sayısını giriniz: "))
 
-def donembasiStokHesapla(koltukSayisi,yatakSayisi,dolapSayisi):
+def donembasiStokHesapla(dbasi_koltukSayisi,dbasi_yatakSayisi,dbasi_dolapSayisi):
     global donembasiStok
-    donembasiStok=koltukSayisi+yatakSayisi+dolapSayisi
+    donembasiStok=dbasi_koltukSayisi+dbasi_yatakSayisi+dbasi_dolapSayisi
     return donembasiStok
 
 
-dsonu_koltuk=25
-dsonu_yatak=20
-dsonu_dolap=10
+dsonu_koltukSayisi=25
+dsonu_yatakSayisi=20
+dsonu_dolapSayisi=10
 
-def donemsonuStokHesapla(dsonu_koltuk,dsonu_yatak,dsonu_dolap):
+def donemsonuStokHesapla(dsonu_koltukSayisi,dsonu_yatakSayisi,dsonu_dolapSayisi):
     global donemsonuStok
-    donemsonuStok=dsonu_koltuk+dsonu_yatak+dsonu_dolap
+    donemsonuStok=dsonu_koltukSayisi+dsonu_yatakSayisi+dsonu_dolapSayisi
     return donemsonuStok
 
 def ortalamaStokHesapla(donembasi,donemsonu):
@@ -22,6 +22,6 @@ def ortalamaStokHesapla(donembasi,donemsonu):
     ortalamaStok=(donembasi + donemsonu) / 2
     print(ortalamaStok)
 
-donembasi=donembasiStokHesapla(koltukSayisi,yatakSayisi,dolapSayisi)
-donemsonu=donemsonuStokHesapla(dsonu_koltuk,dsonu_yatak,dsonu_dolap)
+donembasi=donembasiStokHesapla(dbasi_koltukSayisi,dbasi_yatakSayisi,dbasi_dolapSayisi)
+donemsonu=donemsonuStokHesapla(dsonu_koltukSayisi,dsonu_yatakSayisi,dsonu_dolapSayisi)
 ortalamaStokHesapla(donembasi,donemsonu)
